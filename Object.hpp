1384fe97df2c6c76b5e35582a187499781703403
Firastic <adyaksa.wisanggeni@gmail.com>
2019-03-11 12:05:08 +0700

    Update object

--- Object.hpp
+++ Object.hpp
@@ -13,6 +13,10 @@ public:
 	Object(string _objectType, char _objectSymbol = '?');
 	//Untuk mengeluarkan simbol dari suatu objek ke layar
 	void render();
+	//Virtual method untuk berinteraksi dengan interact
+	virtual void interact() = 0;
+	//Virtual method untuk menentukan apakah suatu objek bisa dilangkahi atau tidak
+	virtual bool isWalkable() = 0;
 
 	//Getter
 	string getObjectType();
