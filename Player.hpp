75f23e3633b4e68044a0fdf591d15621e2ac373c
Ricky Yuliawan Pohan <rickyyuliawan21@gmail.com>
2019-03-12 18:08:28 +0700

    Create Player.hpp

---
+++ Player.hpp
@@ -0,0 +1,45 @@
+// Player.hpp
+
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <string>
+#include "Product.hpp"
+
+#define MAX_WATER 100
+#define MAX_INVENTORY 10
+
+using namespace std;
+
+class Player : public Product{
+	public:
+		// ctor tanpa parameter
+		Player();		
+		
+		// ctor dengan parameter
+		Player(string _name, int _water, Product* _inventory); 
+		
+		// getter
+		string getName();
+		int getWater();
+		Product* getInventory();
+		
+		// setter
+		setName(string _name);
+		setWater(int _water);
+		setInventory(Product* _tas);
+		
+		// command
+		void Move();
+		void Talk();
+		void Interact();
+		void Kill();
+		void Grow();
+		void Mix();
+	private:
+		string player_name; //nama pemain
+		int water;			//kapasitas air
+        Product* inventory; //tas/inventory
+};
+
+#endif
