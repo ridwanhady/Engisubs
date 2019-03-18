de57bdef23489c334b2ef91037fa9d79b33e0022
Ricky Yuliawan Pohan <rickyyuliawan21@gmail.com>
2019-03-18 11:15:49 +0700

    add getter, setter

--- Dog.hpp
+++ Dog.hpp
@@ -1,14 +1,26 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
 #include "FarmAnimal.hpp"
 #include "DogMeat.hpp"
+#include <string>
 
 class Dog : public FarmAnimal {
     private:
-
+    string name;
+    
     public:
         // ctor
         Dog();    
+        
+        // ctor dengan parameter
+        Dog(const string _name);
+    
+        // getter
+        setName( _name);
+    
+        // setter
+        getName(); 
     
         /* Method kill will produce its Animal Meat
          * if applied on Dog, it will produce Dog Meat.
