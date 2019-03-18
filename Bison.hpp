a3e926dc0b7fbeda79ebaa9ae7e184d2941e26cc
Ricky Yuliawan Pohan <rickyyuliawan21@gmail.com>
2019-03-18 11:12:15 +0700

    add getter and setter

--- Bison.hpp
+++ Bison.hpp
@@ -1,17 +1,29 @@
 #ifndef _BISON_H_
 #define _BISON_H_
+
 #include "FarmAnimal.hpp"
 #include "BisonMeat.hpp"
 #include "BisonMilk.hpp"
+#include <string>
 
 class Bison : public FarmAnimal {
     private:
-
+        string name;
+    
     public:
         // ctor
         Bison();
         
-        //
+        // ctor dengan parameter
+        Bison(const string _name);
+    
+        // getter
+        setName( _name);
+    
+        // setter
+        getName();    
+    
+        // produk
         BisonMilk produceMilk();
         BisonMeat kill();
 };
