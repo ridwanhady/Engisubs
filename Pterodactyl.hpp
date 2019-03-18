b33f04efb66514efc12d7623d361ba7d01ee2f64
Ricky Yuliawan Pohan <rickyyuliawan21@gmail.com>
2019-03-18 11:21:16 +0700

    add getter, setter

--- Pterodactyl.hpp
+++ Pterodactyl.hpp
@@ -1,13 +1,25 @@
 #ifndef _PTERODACTYL_H_
 #define _PTERODACTYL_H_
+
 #include "FarmAnimal.hpp"
 #include "PterodactylEgg.hpp"
+#include <string>
 
 class Pterodactyl : public FarmAnimal {
       // ctor
       Pterodactyl();
       
-      // 
+      // ctor dengan parameter
+      Pterodactyl(const string _name);
+      
+        // getter
+        setName( _name);
+    
+        // setter
+        getName(); 
+      
+      
+      // produksi
       PterodactylEgg produceEgg();
 
 };
