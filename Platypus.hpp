8847dcd22844864be573982423c185f8094a9e41
Ricky Yuliawan Pohan <rickyyuliawan21@gmail.com>
2019-03-18 11:20:05 +0700

    add getter, setter

--- Platypus.hpp
+++ Platypus.hpp
@@ -1,18 +1,30 @@
 #ifndef _PLATYPUS_H_
 #define _PLATYPUS_H_
+
 #include "FarmAnimal.hpp"
 #include "PlatypusEgg.hpp"
 #include "PlatypusMeat.hpp"
 #include "PlatypusMilk.hpp"
+#include <string>
 
 class Platypus : public FarmAnimal {
     private:
-
+        string name;
+    
     public:
         // ctor
         Platypus();
+        
+        // ctor dengan parameter
+        Platypus(const string _name);
     
-        //
+        // getter
+        setName( _name);
+    
+        // setter
+        getName(); 
+    
+        //produksi
         PlatypusMilk produceMilk();
         PlatypusEgg produceEgg();
         PlatypusMeat kill();
