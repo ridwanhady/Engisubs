252e68f810e9c22f1c34f82132f4239b0f299d9c
Firastic <adyaksa.wisanggeni@gmail.com>
2019-03-18 09:53:18 +0700

    Resolve conflict

--- FarmProduct.hpp
+++ FarmProduct.hpp
@@ -1,23 +1,15 @@
 #ifndef _FARMPRODUCT_HPP_
 #define _FARMPRODUCT_HPP_
-<<<<<<< HEAD
 #include "Product.hpp"
-=======
-
-#include "Object.hpp"
->>>>>>> be83e45e755009451c7cd26a0d47da5d36ac131f
 
 class FarmProduct : public Product {
     private:
 
     public:
-<<<<<<< HEAD
-=======
     	//Ctor tanpa parameter
         FarmProduct();
         //Virtual dtor
         virtual ~FarmProduct();
->>>>>>> be83e45e755009451c7cd26a0d47da5d36ac131f
 };
 
 #endif
