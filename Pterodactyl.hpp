306103f679462bd42150972aaf018300a625a967
Ricky Yuliawan Pohan <rickyyuliawan21@gmail.com>
2019-03-18 11:02:58 +0700

    add ctor

--- Pterodactyl.hpp
+++ Pterodactyl.hpp
@@ -1,8 +1,14 @@
 #ifndef _PTERODACTYL_H_
 #define _PTERODACTYL_H_
 #include "FarmAnimal.hpp"
+#include "PterodactylEgg.hpp"
 
 class Pterodactyl : public FarmAnimal {
+      // ctor
+      Pterodactyl();
+      
+      // 
+      PterodactylEgg produceEgg();
 
 };
 
