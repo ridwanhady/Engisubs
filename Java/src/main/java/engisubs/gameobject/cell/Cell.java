ead95a42a4c94881ee0768e49e2fd64818ddb557
EdwardAJ <edward.jaya31@gmail.com>
2019-04-15 00:21:28 +0700

    Adding Cell and Land

--- Java/src/main/java/engisubs/gameobject/cell/Cell.java
+++ Java/src/main/java/engisubs/gameobject/cell/Cell.java
@@ -1,7 +1,45 @@
 package engisubs.gameobject.cell;
-
-import engisubs.gameobject.GameObject;;
+import engisubs.gameobject.GameObject;
 
 public abstract class Cell extends GameObject{
+    protected int positionX;
+    protected int positionY;
+
+    /** 
+     * Konstruktor dari Cell dengan parameter.
+     */
+
+    public Cell(int _positionX, int _positionY) {
+        setPosition(_positionX, _positionY);
+    }
+
+    /**
+     * Method untuk mengecek apakah suatu petak dapat dilangkahi player atau tidak
+     */
+    public boolean isWalkable() {
+        return false;
+    }
+
+    /**
+     * Getter
+     */
+
+    public int getPositionX(){
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    /** 
+     * Setter
+     */
+    public void setPosition(int _positionX, int _positionY) {
+        positionX = _positionX;
+        positionY = _positionY;
+    }
 
 }
+
+
