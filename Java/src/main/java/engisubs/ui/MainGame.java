5d40c14dfdcb6e533f4eb93a375c0de01f1ba995
EdwardAJ <edward.jaya31@gmail.com>
2019-04-19 22:13:10 +0700

    Adding button (no func yet)

--- Java/src/main/java/engisubs/ui/MainGame.java
+++ Java/src/main/java/engisubs/ui/MainGame.java
@@ -27,17 +27,41 @@ public class MainGame {
     }
 
     public MainGame() {
-        JButton up = new JButton("Up");
-        Font up_font  = new Font(Font.SANS_SERIF, Font.BOLD, 20);
-        up.setFont(up_font);
-        up.setBounds(1000,50,100,50);
-        up.setBackground(Color.white);
 
         frmClass = new JFrame();
+        JButton up = new JButton("Up");
+        JButton down = new JButton("Down");
+        JButton left = new JButton("Left");
+        JButton right = new JButton("Right");
+
+        Font font  = new Font(Font.SANS_SERIF, Font.BOLD, 13);
+   
+
+        up.setFont(font);
+        up.setBounds(1020,250,60,40);
+        up.setBackground(Color.white);
+
+        down.setFont(font);
+        down.setBounds(1020,350,60,40);
+        down.setBackground(Color.white);
+
+        left.setFont(font);
+        left.setBounds(930,300,60,40);
+        left.setBackground(Color.white);
+
+        right.setFont(font);
+        right.setBounds(1110,300,60,40);
+        right.setBackground(Color.white);
+
+        
         frmClass.setSize(1280,720);
         frmClass.setTitle("Engi's Farm by AwSubs");
         frmClass.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frmClass.add(up); 
+        frmClass.add(down);
+        frmClass.add(left);
+        frmClass.add(right);
+        frmClass.setLayout(null);
     }
 
 }
